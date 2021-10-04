4dc9b9c568a330eeb3d04273795965717b418fd8
Nora Berisha <nora.berisha@openresearch.com>
2021-10-04 22:47:13 +0200

    changed table view cell xib

--- tictactoe/HistoryController.swift
+++ tictactoe/HistoryController.swift
@@ -9,6 +9,7 @@ import UIKit
 import Foundation
 import RealmSwift
 
+
 class HistoryController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let realm = try! Realm()
@@ -28,21 +29,25 @@ func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> 
 
 func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "HistoryTableViewCell", for: indexPath) as! HistoryTableViewCell
-    cell.lblWinner.text = myData[indexPath.row]
-    cell.lblDate.text = myData[indexPath.row]
+    let formatter = DateFormatter()
+    formatter.dateFormat = "HH:mm E, d MMM y"
+    cell.lblWinner.text = myData[indexPath.row].winner
+    cell.lblDate.text = formatter.string(from: myData[indexPath.row].date!)
     return cell
 }
     
 }
 
 class HelperClass {
-    static func test() -> List<String> {
-        let winners = try! Realm().objects(TicTacToeDb.self)
-        let test: List<String> = List()
-        for winner in winners {
-            let testi = winner.winner ?? "lala" + " has won on " + (winner.date?.description)!
-            test.append(testi)
+    static func test() -> List<TicTacToeDb> {
+        let winnersResult = try! Realm().objects(TicTacToeDb.self)
+        let winners: List<TicTacToeDb> = List()
+        for result in winnersResult{
+            let ticTacToeDb = TicTacToeDb()
+            ticTacToeDb.winner = result.winner
+            ticTacToeDb.date = result.date
+            winners.append(ticTacToeDb)
         }
-        return test
+        return winners
     }
 }
